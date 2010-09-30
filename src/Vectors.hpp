7527f3c5e65938921c01c10667e79ac482ec4d4c
Nicolas Bigaouette <nbigaouette@gmail.com>
2010-09-30 14:20:15 -0400

    static inline to template.

--- src/Vectors.hpp
+++ src/Vectors.hpp
@@ -6,50 +6,58 @@
 // **************************************************************
 //                      Vector operations
 // **************************************************************
-static inline void Vector_Cross_Product(double a[3], const double b[3], const double c[3])
+template <class Double>
+void Vector_Cross_Product(Double a[3], const Double b[3], const Double c[3])
 {
     a[0] = b[1]*c[2] - b[2]*c[1];
     a[1] = b[2]*c[0] - b[0]*c[2];
     a[2] = b[0]*c[1] - b[1]*c[0];
 }
 
-static inline double Vector_Dot_Product(const double a[3], const double b[3])
+template <class Double>
+Double Vector_Dot_Product(const Double a[3], const Double b[3])
 {
     return (a[0]*b[0] + a[1]*b[1] + a[2]*b[2]);
 }
 
-static inline double Vector_Length_Squared(const double a[3])
+template <class Double>
+Double Vector_Length_Squared(const Double a[3])
 {
     return Vector_Dot_Product(a, a);
 }
 
-static inline double Vector_Length(const double a[3])
+template <class Double>
+Double Vector_Length(const Double a[3])
 {
     return sqrt(Vector_Length_Squared(a));
 }
 
-static inline void Vector_Times_Scalar(double a[3], const double b[3], const double c)
+template <class Double>
+void Vector_Times_Scalar(Double a[3], const Double b[3], const Double c)
 {
     a[0] = b[0] * c;
     a[1] = b[1] * c;
     a[2] = b[2] * c;
 }
 
-static inline void Vector_Add(double a[3], const double b[3], const double c[3])
+template <class Double>
+void Vector_Add(Double a[3], const Double b[3], const Double c[3])
 {
     a[0] = b[0] + c[0];
     a[1] = b[1] + c[1];
     a[2] = b[2] + c[2];
 }
 
-static inline void Vector_Substract(double a[3], const double b[3], const double c[3])
+template <class Double>
+void Vector_Substract(Double a[3], const Double b[3], const Double c[3])
 {
     a[0] = b[0] - c[0];
     a[1] = b[1] - c[1];
     a[2] = b[2] - c[2];
 }
 
-static inline double Vector_Substract_and_Length_Squared(double a[3], const double b[3])
+template <class Double>
+Double Vector_Substract_and_Length_Squared(Double a[3], const Double b[3])
 {
     return (
               (a[0] - b[0])*(a[0] - b[0])
@@ -57,10 +65,6 @@ static inline double Vector_Substract_and_Length_Squared(double a[3], const doub
             + (a[2] - b[2])*(a[2] - b[2])
         );
 }
-
-
-
-
 
 // **************************************************************
 template <class Double>
