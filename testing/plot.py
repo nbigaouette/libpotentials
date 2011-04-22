610f778f31d808e5dec474bae7eb18b4a63bd4ae
Nicolas Bigaouette <nbigaouette@gmail.com>
2011-04-22 13:39:15 -0400

    Script to plot data.

---
+++ testing/plot.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import sys, os, glob
+import numpy
+import matplotlib.pyplot as plt
+
+import on_key
+
+globber = glob.glob(os.path.join("output", "*"))
+
+fig = on_key.figure()
+ax1 = plt.subplot(211)
+ax2 = plt.subplot(212)
+
+for folder in globber:
+
+    # Skip files
+    if (os.path.isfile(folder)):
+        continue
+
+    field_files = glob.glob(os.path.join(folder, "field_*"))
+    field_files.sort()
+
+    pot_files = glob.glob(os.path.join(folder, "pot*"))
+    pot_files.sort()
+
+    assert(len(field_files) == len(pot_files))
+    nb_cs = len(field_files)
+
+    for cs in xrange(nb_cs):
+        data = numpy.loadtxt(pot_files[cs], delimiter=',', skiprows=0, dtype=float)
+        r     = data[:,0]
+        pot   = data[:,1]
+
+        ax1.plot(r, pot, label = folder.replace("output/", ""))
+
+    for cs in xrange(nb_cs):
+        data = numpy.loadtxt(field_files[cs], delimiter=',', skiprows=0, dtype=float)
+        r     = data[:,0]
+        field = data[:,1]
+
+        ax2.plot(r, field, label = folder.replace("output/", ""))
+
+
+ax1.set_ylabel("Potential (au)")
+ax2.set_ylabel("Field (au)")
+ax2.set_xlabel("r (Bohr)")
+
+ax1.set_ylim((0.0, 11.0))
+ax2.set_ylim((-5.0, 1.0))
+
+plt.legend()
+
+plt.show()
+
