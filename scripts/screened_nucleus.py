9985e902e0b8e972acfffaa8990f7ba636f02e71
Nicolas Bigaouette <nbigaouette@gmail.com>
2011-04-25 14:38:55 -0400

    Start of least-square fitting

--- scripts/screened_nucleus.py
+++ scripts/screened_nucleus.py
@@ -5,6 +5,7 @@ import numpy as np
 import math as m
 import matplotlib.pyplot as plt
 from scipy.special import erf
+from scipy.optimize import leastsq
 
 import on_key
 import herman_skillman
@@ -33,6 +34,14 @@ plt.plot(r, z/r+(z-1)*Ve,      label='$V_{+'+str('%d' % z) + '} + '+str('%d' % (
 cs = 1
 for hsi in xrange(herman_skillman.cutoffs.shape[1]):
     plt.plot([herman_skillman.cutoffs[cs+1,hsi], herman_skillman.cutoffs[cs+1,hsi], herman_skillman.cutoffs[cs+1,hsi]], [-1.0, 1.0e-5, 20.0], '--k')
+
+# Least-square fitting
+# http://www.scipy.org/Cookbook/FittingData
+#fitfunc = lambda p, r: -erf(r / (m.sqrt(2)*p[0])) / r # Target function
+#errfunc = lambda p, r, x: fitfunc(p, r) - x # Distance to the target function
+#p0 = [-15., 0.8, 0., -1.] # Initial guess for the parameters
+#p1, success = optimize.leastsq(errfunc, p0[:], args=(Tx, tX))
+
 
 ax1.set_yscale('log')
 ax1.set_ylim((1.0e-2, 10.0))
