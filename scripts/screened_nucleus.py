31a67734c2511e016dff2b6cace233b6a6ff3861
Nicolas Bigaouette <nbigaouette@gmail.com>
2011-04-25 19:28:18 -0400

    Cleanup

--- scripts/screened_nucleus.py
+++ scripts/screened_nucleus.py
@@ -21,37 +21,16 @@ r = np.linspace(1.0, 50.0, 1000) # Bohr
 #r = np.linspace(herman_skillman.cutoffs[2,1], herman_skillman.cutoffs[2,3], 100) # Bohr
 #r = np.linspace(0.5, herman_skillman.cutoffs[2,3], 100) # Bohr
 
-#s = 1.75
-#Ve = Velectron(r, s)
 
 VHS = herman_skillman.HS(r, 1)
 
 fig = on_key.figure()
 ax1 = plt.subplot(111)
 
-#for i in xrange(3):
-    #plt.plot(r, float(i)/r,  '--k')
-    ##plt.plot(r, float(i)/r,  '--k', label='$V_{+'+str(i)+'}$')
-
-#plt.plot(r, Ve,         label='$V_{e}$')
-#z = 8.0
-#plt.plot(r, z/r+(z-1)*Ve,      label='$V_{+'+str('%d' % z) + '} + '+str('%d' % (z-1))+'*V_e$')
-
 # Plot HS's cuttofs
 #cs = 1
 #for hsi in xrange(herman_skillman.cutoffs.shape[1]):
     #plt.plot([herman_skillman.cutoffs[cs+1,hsi], herman_skillman.cutoffs[cs+1,hsi], herman_skillman.cutoffs[cs+1,hsi]], [-1.0, 1.0e-5, 20.0], '--k')
-
-# Least-square fitting
-# http://www.scipy.org/Cookbook/FittingData
-#fitfunc = lambda p, r, Ne: -erf(r / (m.sqrt(2)*p[0])) / r # Target function
-#errfunc = lambda p, r, x: fitfunc(p, r) - x # Distance to the target function
-#p0 = [-15., 0.8, 0., -1.] # Initial guess for the parameters
-#p1, success = optimize.leastsq(errfunc, p0[:], args=(Tx, tX))
-
-#Ne = 3
-#Ne = 54
-#Ne = 14
 
 def Vtot(sigmas, r):
     Vtot = float(Ne+1)/r
