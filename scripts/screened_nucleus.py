99b5634c3d57e4a0e42e27d33c7691b8154753be
Nicolas Bigaouette <nbigaouette@gmail.com>
2011-04-24 23:58:52 -0400

    Plot HS and comparision with charge distribution.

--- scripts/screened_nucleus.py
+++ scripts/screened_nucleus.py
@@ -7,31 +7,36 @@ import matplotlib.pyplot as plt
 from scipy.special import erf
 
 import on_key
+import herman_skillman
 
+r = np.linspace(1.0e-1, 20.0, 100) # Bohr
 
-r = np.linspace(1.0e-1, 10.0, 1000) # Bohr
+s = 1.75
 
-#Z = 54
-#sigmas = [1.0, 0.5, 0.25]
-sigmas = [1.0, 0.5, 0.25]
-
-V1 = 1.0 / r
-Vn = len(sigmas) * V1
-Ve = [
-        -erf(r/(m.sqrt(2) * sigmas[0]))/r,
-        -erf(r/(m.sqrt(2) * sigmas[1]))/r,
-        -erf(r/(m.sqrt(2) * sigmas[2]))/r
-    ]
-
+Ve = -erf(r/(m.sqrt(2) * s))/r
+VHS = herman_skillman.HS(r, 1)
 
 fig = on_key.figure()
+ax1 = plt.subplot(111)
 
-plt.plot(r, V1,     '--', label='$V_{+1}$')
-plt.plot(r, Vn,     label='$V_{+' + str(len(sigmas)) + '}$')
-plt.plot(r, Ve[0],  label='$V_{e,0}$')
-plt.plot(r, Ve[1],  label='$V_{e,1}$')
-plt.plot(r, Ve[2],  label='$V_{e,2}$')
-plt.plot(r, Vn+Ve[0]+Ve[1]+Ve[2],  label='$V_{+' + str(len(sigmas)) + '} + \sum V_e$')
+plt.plot(r, VHS,        label='$V_{HS}$')
+for i in xrange(10):
+    plt.plot(r, float(i)/r,  '--k')
+    #plt.plot(r, float(i)/r,  '--k', label='$V_{+'+str(i)+'}$')
 
+plt.plot(r, Ve,         label='$V_{e}$')
+#plt.plot(r, V2+Ve,      label='$V_{+2} + V_e$')
+plt.plot(r, 10.0/r+9.0*Ve,      label='$V_{+10} + 9*V_e$')
+
+# Plot HS's cuttofs
+cs = 1
+for hsi in xrange(herman_skillman.cutoffs.shape[1]):
+    plt.plot([herman_skillman.cutoffs[cs+1,hsi], herman_skillman.cutoffs[cs+1,hsi], herman_skillman.cutoffs[cs+1,hsi]], [-1.0, 1.0e-5, 20.0], '--k')
+
+ax1.set_yscale('log')
+ax1.set_ylim((1.0e-2, 10.0))
+#ax1.set_ylim((-2.0, 10.0))
 plt.legend()
 plt.show()
+
+
