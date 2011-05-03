496397fbf3eb3dafb153ec55cda94f09ef36040e
Edward Ackad <eackad@gmail.com>
2011-05-03 12:42:34 -0400

    Added missing file

---
+++ src/Version.hpp
@@ -0,0 +1,8 @@
+#ifndef INC_LIBPOTENTIALS_VERSION_hpp
+#define INC_LIBPOTENTIALS_VERSION_hpp
+namespace libpotentials {
+    const char *const build_time = "Mon May  2 15:52:12 EDT 2011";
+    const char *const build_sha = "master";
+    const char *const build_branch = "a622c29d902b7f88f0d9b49bffa7eebf37ba2b14";
+}
+#endif // #ifndef INC_LIBPOTENTIALS_VERSION_hpp
