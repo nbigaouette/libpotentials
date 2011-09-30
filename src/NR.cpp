090d988bd2c48cee9297ec5564c5fd341c673cb3
Nicolas Bigaouette <nbigaouette@gmail.com>
2011-09-30 09:59:41 -0400

    Cast to fdouble to clean up warnings.

--- src/NR.cpp
+++ src/NR.cpp
@@ -23,6 +23,9 @@
 namespace nr
 {
 
+    const fdouble fone  = fdouble(1.0);
+    const fdouble fhalf = fdouble(0.5);
+
 // **************************************************************
 fdouble Gamma_NaturalLogarithm(fdouble xx)
 /**
@@ -35,21 +38,21 @@ fdouble Gamma_NaturalLogarithm(fdouble xx)
     fdouble cof[6],stp;
     fdouble x,y,tmp,ser;
 
-    cof[0] = 76.18009172947146;
-    cof[1] = -86.50532032941677;
-    cof[2] = 24.01409824083091;
-    cof[3] = -1.231739572450155;
-    cof[4] = 0.1208650973866179E-2;
-    cof[5] = -0.5395239384953E-5;
+    cof[0] = fdouble(76.18009172947146);
+    cof[1] = fdouble(-86.50532032941677);
+    cof[2] = fdouble(24.01409824083091);
+    cof[3] = fdouble(-1.231739572450155);
+    cof[4] = fdouble(0.1208650973866179E-2);
+    cof[5] = fdouble(-0.5395239384953E-5);
 
-    stp = 2.5066282746310005;
+    stp = fdouble(2.5066282746310005);
 
     x = xx;
     y = x;
-    tmp = x + 5.5;
+    tmp = x + fdouble(5.5);
     tmp = (x+fhalf)*std::log(tmp) - tmp;
 
-    ser = 1.000000000190015;
+    ser = fdouble(1.000000000190015);
 
     for( i = 0; i < 6; i++ )
     {
@@ -81,19 +84,19 @@ fdouble gammln(fdouble xx)
     // a nicety that you can omit if ﬁve-ﬁgure accuracy is good enough.
     fdouble x, y, tmp, ser;
     static fdouble cof[6] = {
-        76.18009172947146,
-        -86.50532032941677,
-        24.01409824083091,
-        -1.231739572450155,
-        0.1208650973866179e-2,
-        -0.5395239384953e-5
+        fdouble(76.18009172947146),
+        fdouble(-86.50532032941677),
+        fdouble(24.01409824083091),
+        fdouble(-1.231739572450155),
+        fdouble(0.1208650973866179e-2),
+        fdouble(-0.5395239384953e-5)
     };
 
     int j;
     y = x = xx;
-    tmp = x + 5.5;
-    ser = 1.000000000190015;
+    tmp = x + fdouble(5.5);
     tmp -= (x + fhalf) * std::log(tmp);
+    ser = fdouble(1.000000000190015);
     for (j = 0 ; j <= 5 ; j++) ser += cof[j] / ++y;
 
     return -tmp + std::log(fdouble(2.5066282746310005) * ser / x);
@@ -114,7 +117,7 @@ fdouble gammp(fdouble a, fdouble x)
         abort();
     }
 
-    if (x < (a+1.0))
+    if (x < (a+fone))
     {
         //Use the series representation.
         gser(&gamser, a, x, &gln);
@@ -122,7 +125,7 @@ fdouble gammp(fdouble a, fdouble x)
     } else {
         // Use the continued fraction representation and take its complement.
         gcf(&gammcf, a, x, &gln);
-        return 1.0 - gammcf;
+        return fone - gammcf;
     }
 }
 
@@ -136,7 +139,7 @@ void gser(fdouble *gamser, fdouble a, fdouble x, fdouble *gln)
  */
 {
     const int    ITMAX = 100;
-    const fdouble EPS   = 3.0e-7;
+    const fdouble EPS   = fdouble(3.0e-7);
 
     int n;
     fdouble sum, del, ap;
@@ -154,7 +157,7 @@ void gser(fdouble *gamser, fdouble a, fdouble x, fdouble *gln)
         return;
     } else {
         ap = a;
-        del = sum = 1.0 / a;
+        del = sum = fone / a;
         for (n = 1 ; n <= ITMAX ; n++)
         {
             ++ap;
@@ -184,28 +187,28 @@ void gcf(fdouble *gammcf, fdouble a, fdouble x, fdouble *gln)
     fdouble an, b, c, d, del, h;
 
     const int    ITMAX = 100;       // Maximum allowed number of iterations.
-    const fdouble EPS   = 3.0e-7;    // Relative accuracy.
-    const fdouble FPMIN = 1.0e-30;   // Number near the smallest representable ﬂoating-point number.
+    const fdouble EPS   = fdouble(3.0e-7);    // Relative accuracy.
+    const fdouble FPMIN = fdouble(1.0e-30);   // Number near the smallest representable ﬂoating-point number.
 
     *gln = gammln(a);
 
     // Set up for evaluating continued fraction by modiﬁed
     // Lentz's method (§5.2) with b0 = 0.
-    b = x + 1.0 - a;
-    c=1.0 / FPMIN;
-    d=1.0 / b;
+    b = x + fone - a;
+    c=fone / FPMIN;
+    d=fone / b;
 
     h = d;
     for (i = 1 ; i <= ITMAX ; i++)
     {
         // Iterate to convergence.
-        an = -i*(i-a);
-        b += 2.0;
+        an = -fdouble(i)*(fdouble(i)-a);
+        b += fdouble(2.0);
         d = an * d + b;
         if (std::abs(d) < FPMIN) d = FPMIN;
         c = b + an / c;
         if (std::abs(c) < FPMIN) c = FPMIN;
-        d = 1.0 / d;
+        d = fone / d;
         del = d * c;
         h *= del;
         if (std::abs(del-fone) < EPS) break;
@@ -239,7 +242,7 @@ fdouble int_erf(fdouble x)
 {
     int loop,n=1000; /* number of points */
     fdouble I,h;
-    h=(fdouble)x/(n-1);
+    h=x/fdouble(n-1);
     I=0;
     I+=fdouble(3.0/8.0)  *h;
     I+=fdouble(7.0/6.0)  *h*std::exp(-h*h);
@@ -259,20 +262,20 @@ fdouble python_erf(fdouble x)
  */
 {
     // constants
-    const fdouble a1 =  0.254829592;
-    const fdouble a2 = -0.284496736;
-    const fdouble a3 =  1.421413741;
-    const fdouble a4 = -1.453152027;
-    const fdouble a5 =  1.061405429;
-    const fdouble p  =  0.3275911;
+    const fdouble a1 = fdouble( 0.254829592);
+    const fdouble a2 = fdouble(-0.284496736);
+    const fdouble a3 = fdouble( 1.421413741);
+    const fdouble a4 = fdouble(-1.453152027);
+    const fdouble a5 = fdouble( 1.061405429);
+    const fdouble p  = fdouble( 0.3275911);
 
     // Save the sign of x
-    fdouble sign = 1.0;
-    if (x < 0.0) sign = -1.0;
+    fdouble sign = fone;
+    if (x < 0.0) sign = -fone;
     const fdouble absx = std::abs(x);
 
     // A & S 7.1.26
-    const fdouble t = 1.0 / (1.0 + p * absx);
+    const fdouble t = fone / (fone + p * absx);
     const fdouble y = fone - (((((a5*t + a4)*t) + a3)*t + a2)*t + a1)*t * std::exp(-absx*absx);
 
     return sign*y;
