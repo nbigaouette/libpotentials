613ddb9a41172ce01f1e2881e8f5d5581202c7d8
Nicolas Bigaouette <nbigaouette@gmail.com>
2011-10-24 10:51:23 -0400

    What is stored is the potential divided by the charge state. Don't multiply the potential by the charge state when finding the distance where HS == base.

--- src/HermanSkillman.cpp
+++ src/HermanSkillman.cpp
@@ -212,7 +212,7 @@ void Initialize_HS(const fdouble &base_potential)
             pot = std::abs(hs_lut_potential[cs_i].read(found_r));
             //printf("base_potential = %10.5g   r_left = %10.5g   r = %10.5g   r_right = %10.5g   HS(r) = %10.5g\n", base_potential, r_left, found_r, r_right, pot);
             Assert_isinf_isnan(pot);
-            if (pot <= fdouble(std::max(1,cs))*base_potential)
+            if (pot <= base_potential)
             {
                 r_right = found_r;
             }
